2769d942d5614d9f06b78c5585bc2d02b184a2a6
Elfarina Fadma Sucitra <12350122929@students.uin-suska.ac.id>
2024-09-10 17:44:50 +0700

    Add files via upload

---
+++ Profil.java
@@ -0,0 +1,15 @@
+package Tugas;
+
+public class Profil {
+    public static void main(String[] args) {
+        String nama = "Elfarina Fadma Sucitra";
+        String nim = "12350122929";
+        String hobbi = "Menonton dan olahraga";
+        System.out.println("        BIODATA DIRI");
+        System.out.println("=================================");
+        System.out.println("Nama   : " + nama);
+        System.out.println("NIM    : " + nim);
+        System.out.println("hobbi  : " + hobbi);
+        System.out.println("=================================");
+    }
+}
